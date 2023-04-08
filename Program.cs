da85ebbcfe949f6858c5947f5a29552edd8ef9c0
samirasingh19 <samira.singh021@gmail.com>
2023-04-08 09:24:16 +0530

    EmployeeWage NonStatic

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace EmployeeWageNonStatic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to employee wage computation problem");
+
+
+
+            // EmployeeAttendence attendence = new EmployeeAttendence();
+
+            // EmployeeAttendence.EmpAttendance();
+
+            //EmployeeCheck.EmployeeWage();
+            //EmployeeCheck check = new EmployeeCheck();
+
+            // PartTimeEmployee.PartTimeEmployeeWage();
+            // PartTimeEmployee employee = new PartTimeEmployee();
+
+            // EmployeeWageSwitcCase.EmployeeWageSwitchCaseStatement();
+            // EmployeeWageSwitcCase switcCase = new EmployeeWageSwitcCase();
+
+            // WagesForMonth.WagesforaMonth();
+            // WagesForMonth month = new WagesForMonth();
+
+
+            // EmployeeWageCondition.EmployeeWageConditionForWorkingHours();
+            //  EmployeeWageCondition condition = new EmployeeWageCondition();
+
+
+        }
+    }
+}
